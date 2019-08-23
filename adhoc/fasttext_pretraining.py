64b69d360bb2154e92202983b8f2deb9a72cf404
Conor Sheehan <ConorJamesSheehan@gmail.com>
2019-08-23 11:49:02 +0100

    fixed fasttext pretrain script

--- adhoc/fasttext_pretraining.py
+++ adhoc/fasttext_pretraining.py
@@ -1,9 +1,10 @@
 import os
+from pkg_resources import resource_filename
 from fasttext import train_supervised
 
-TEXTBOOK_PATH = os.path.join('..', 'glycobiology_tsne', 'data', 'textbook.txt')
+TEXTBOOK_PATH = os.path.join(resource_filename('glycobiology_tsne', 'data'), 'textbook.txt')
 
 
 def train_model(pretrained_weights, output_path):
-    model = train_supervised(pretrainedVectors=pretrained_weights)
+    model = train_supervised(TEXTBOOK_PATH, pretrainedVectors=pretrained_weights)
     model.save_model(output_path)
