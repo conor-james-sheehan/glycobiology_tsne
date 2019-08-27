f0b01226a45d1bc78f69764559949ab7e0006452
Conor Sheehan <ConorJamesSheehan@gmail.com>
2019-08-27 12:00:37 +0100

    added params for fasttext pretraining

--- adhoc/fasttext_pretraining.py
+++ adhoc/fasttext_pretraining.py
@@ -5,7 +5,7 @@ from fasttext import train_supervised
 CORPUS_PATH = os.path.join(resource_filename('glycobiology_tsne', 'data'), 'glyco_corpus.txt')
 
 
-def train_model(output_path):
+def train_model(output_path, **train_params):
     assert os.path.exists(CORPUS_PATH)
-    model = train_supervised(CORPUS_PATH)
+    model = train_supervised(CORPUS_PATH, **train_params)
     model.save_model(output_path)
